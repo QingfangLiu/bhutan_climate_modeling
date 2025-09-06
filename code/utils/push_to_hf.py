352b4e176e9e8b70a77106b74e0cce365b268632
QL_mac_mini <psychliuqf@gmail.com>
2025-09-06 06:45:00 -0400

    updated push to hf code to upload hydrosheds

--- code/utils/push_to_hf.py
+++ code/utils/push_to_hf.py
@@ -6,13 +6,13 @@ login()
 
 # --- 2) Set repo info ---
 REPO_ID = "qlk0610/bhutan-climate"  # change to your repo
-IS_PRIVATE = True                   # now this repo is private but may change later
+IS_PRIVATE = False                   # now this repo is private but may change later
 
 # --- 3) Upload the local folder ---
 upload_folder(
-    folder_path="../../data/era5",  # local folder you want to upload
-    path_in_repo="era5",      # where it appears inside the HF repo
+    folder_path="../../data/HydroSHEDS",  # local folder you want to upload
+    path_in_repo="HydroSHEDS",      # where it appears inside the HF repo
     repo_id=REPO_ID,
     repo_type="dataset",
-    commit_message="Add ERA5 data (GRIB→Excel→merged Parquet)" # update commit message
+    commit_message="Add HydroSHEDS data" # update commit message
 )
